85836d929020025cb81ebbd9867c4813a657cc02
Paulo Henrique <ph.1928@hotmail.com>
2024-08-30 12:05:03 -0300

    feat: add register route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/pages/login/login.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { RegisterComponent } from './components/pages/register/register.component';
 
 export const routes: Routes = [
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
     {
         path: 'login',
         component: LoginComponent
