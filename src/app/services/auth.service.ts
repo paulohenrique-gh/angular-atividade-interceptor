e5c96cfc5f06e1a414ef02931aaec325e9b548c3
Paulo Henrique <ph.1928@hotmail.com>
2024-08-30 12:08:45 -0300

    feat: add register method to AuthService

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,18 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { User } from '../models/user';
+import { UserLogin } from '../models/user-login';
 import { Observable } from 'rxjs';
 import { TokenResponse } from '../models/token-response';
+import { UserRegistration } from '../models/user-registration';
+import { RegistrationResponse } from '../models/registration-response';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  url = 'http://localhost:8080/auth/'
+  private url = 'http://localhost:8080/auth/';
+  private availableRoles = ['ADMIN', 'DOCTOR', 'RECEPTIONIST', 'PATIENT'];
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
-  login(user: User): Observable<TokenResponse> {
+  getAvailableRoles(): string[] {
+    return this.availableRoles;
+  }
+
+  login(user: UserLogin): Observable<TokenResponse> {
     return this.httpClient.post<TokenResponse>(`${this.url}login`, user);
+  }
+
+  register(user: UserRegistration): Observable<RegistrationResponse> {
+    return this.httpClient.post<RegistrationResponse>(`${this.url}register`, user);
   }
 }
