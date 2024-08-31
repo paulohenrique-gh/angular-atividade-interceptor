9d83d6e9e83ecfb8a5fa8d2bb62d533b2e62dc99
Paulo Henrique <ph.1928@hotmail.com>
2024-08-31 20:59:35 -0300

    refactor: handle login observable in authService

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,10 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { UserLogin } from '../models/user-login';
-import { Observable } from 'rxjs';
+import { catchError, Observable, tap, throwError } from 'rxjs';
 import { TokenResponse } from '../models/token-response';
 import { UserRegistration } from '../models/user-registration';
 import { RegistrationResponse } from '../models/registration-response';
+import { LoggedInUser } from '../models/logged-in-user';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root',
@@ -20,7 +22,16 @@ export class AuthService {
   }
 
   login(user: UserLogin): Observable<TokenResponse> {
-    return this.httpClient.post<TokenResponse>(`${this.url}login`, user);
+    return this.httpClient.post<TokenResponse>(`${this.url}login`, user).pipe(
+      tap((res: TokenResponse) => {
+        console.log('na autenticação', res);
+        localStorage.setItem("token", res.token);
+        return res;
+      }),
+      catchError(error => {
+        return throwError(() => error);
+      })
+    );
   }
 
   register(user: UserRegistration): Observable<RegistrationResponse> {
