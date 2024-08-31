c66404a8604ba0590c8999af6d7c0c7658c9d8a4
Paulo Henrique <ph.1928@hotmail.com>
2024-08-31 20:58:53 -0300

    refactor: Update interceptor name to requestInterceptor

---
+++ src/app/interceptors/request.interceptor.ts
@@ -0,0 +1,15 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const requestInterceptor: HttpInterceptorFn = (req, next) => {
+  if (localStorage.getItem('token')) {
+    const token = localStorage.getItem('token');
+    const clonedReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return next(clonedReq);
+  }
+
+  return next(req);
+};
