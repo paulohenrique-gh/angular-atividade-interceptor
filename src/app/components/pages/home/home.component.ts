f2b0188f725038f964385d8301107eda0763b6ca
Paulo Henrique <ph.1928@hotmail.com>
2024-08-31 20:56:31 -0300

    feat: home component calls userService to load current user

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../services/user.service';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { LoggedInUser } from '../../../models/logged-in-user';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  currentUser$!: Observable<LoggedInUser | null>;
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.loadCurrentUser();
+    this.currentUser$ = this.userService.getCurrentUserObservable();
+  }
 }
