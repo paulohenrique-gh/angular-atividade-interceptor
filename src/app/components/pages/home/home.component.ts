4537cb77f61d8b9b26bbe5fc841b32dce2215b38
Paulo Henrique <ph.1928@hotmail.com>
2024-09-01 15:06:44 -0300

    refactor: HomeComponent onInit subscription to currentUser$
    unsubscribe to currentUser$ on component destroy
    
    add logout method

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,23 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { UserService } from '../../../services/user.service';
 import { AsyncPipe, CommonModule } from '@angular/common';
+import { delay, Subscription } from 'rxjs';
+import { Router, RouterModule } from '@angular/router';
 import { LoggedInUser } from '../../../models/logged-in-user';
-import { Observable } from 'rxjs';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AsyncPipe, CommonModule],
+  imports: [AsyncPipe, CommonModule, RouterModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit {
-  currentUser$!: Observable<LoggedInUser | null>;
+export class HomeComponent implements OnInit, OnDestroy {
+  currentUserSubscription!: Subscription | null;
+  user!: LoggedInUser | null | undefined;
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
     this.userService.loadCurrentUser();
-    this.currentUser$ = this.userService.getCurrentUserObservable();
+    this.currentUserSubscription = this.userService
+      .getCurrentUserObservable()
+      .pipe(delay(2000))
+      .subscribe({
+        next: (data: LoggedInUser | null) => {
+          this.user = data;
+        },
+        error: (error) => {
+          console.log('Error logging in: ', error);
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.currentUserSubscription?.unsubscribe();
+  }
+
+  onLogout(): void {
+    this.authService.logout();
+    this.router.navigate(['login']);
   }
 }
