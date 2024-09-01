a41f9f2f9195de8223482d2a6a596444612849dd
Paulo Henrique <ph.1928@hotmail.com>
2024-08-31 21:00:03 -0300

    feat: create UserService to provide user information

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { LoggedInUser } from '../models/logged-in-user';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, catchError, Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private url = `http://localhost:8080/user/my-info`;
+  private currentUserSubject = new BehaviorSubject<LoggedInUser | null>(null);
+  private currentUser$ = this.currentUserSubject.asObservable();
+
+  constructor(private httpClient: HttpClient) {}
+
+  loadCurrentUser(): void {
+    this.httpClient
+      .get<LoggedInUser>(this.url)
+      .pipe(
+        tap((user) => this.currentUserSubject.next(user)),
+        catchError(error => {
+          this.currentUserSubject.next(null);
+          throw error;
+        })
+      ).subscribe({
+        error: (error) => console.log(error);
+      })
+  }
+
+  getCurrentUserObservable(): Observable<LoggedInUser | null> {
+    return this.currentUser$;
+  }
+}
