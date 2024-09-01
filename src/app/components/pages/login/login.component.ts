d4e734489756c44b6adc1dade9b668a29b633688
Paulo Henrique <ph.1928@hotmail.com>
2024-09-01 15:08:27 -0300

    refactor: add host config to LoginComponent
    add alert for displaying error in login observable

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -3,14 +3,15 @@ import { FormsModule } from '@angular/forms';
 import { UserLogin } from '../../../models/user-login';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../../services/auth.service';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
+  host: {'class': 'form-container'}
 })
 export class LoginComponent {
   user: UserLogin = {
@@ -26,7 +27,7 @@ export class LoginComponent {
         next: () => {
           this.router.navigate(['home']);
         },
-        error: () => alert("Invalid user or password")
+        error: () => alert('Invalid username or password')
       }
     );
   }
